a891f895e8bc658e949fa36d5516e4a1cb699d21
Timothe Duquesne <timothe.duquesne@gmail.com>
2025-08-11 22:29:27 +0200

    feat : added form validation + error management / display

--- scripts/config.js
+++ scripts/config.js
@@ -6,6 +6,10 @@ export const CONFIG = {
   API_URL: "http://localhost:5678/api",
   SELECTORS: {
     GALLERY: ".gallery",
-    FILTERS: ".filters"
+    FILTERS: ".filters",
+    LOGIN: ".login",
+    EMAIL: ".login #email",
+    PASSWORD: ".login #password",
+    ERROR: ".login .error"
   },
 };
---
+++ scripts/login-page.js
@@ -0,0 +1,54 @@
+import { CONFIG } from "./config.js";
+
+function initializeLoginPage() {
+  addSubmitEventListener();
+}
+
+function addSubmitEventListener() {
+  const loginForm = document.querySelector(CONFIG.SELECTORS.LOGIN);
+  loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    handleSubmit();
+  });
+}
+
+function handleSubmit() {
+  const email = document.querySelector(CONFIG.SELECTORS.EMAIL).value;
+  const password = document.querySelector(CONFIG.SELECTORS.PASSWORD).value;
+
+  const areCredentialsOk = verifyInputs(email, password);
+  if (!areCredentialsOk) {
+    displayError("E-mail et / ou mot de passe mal renseigné.");
+  } else {
+  }
+}
+
+function verifyInputs(email, password) {
+  const regExpression = new RegExp(
+    "^[a-zA-Z0-9]([a-zA-Z0-9._-]*[a-zA-Z0-9])?@[a-zA-Z0-9]([a-zA-Z0-9.-]*[a-zA-Z0-9])?.([a-zA-Z]{2,})$"
+  );
+  if (regExpression.test(email) && password.length > 2) return true;
+  else return false;
+}
+
+function displayError(errorMessage) {
+  let errorDisplay = document.querySelector(CONFIG.SELECTORS.ERROR)
+  console.log(errorDisplay)
+  if (errorDisplay == null) {
+    errorDisplay = createErrorDiv()
+  }
+
+  errorDisplay.innerText = errorMessage
+}
+
+function createErrorDiv() {
+  const loginForm = document.querySelector(CONFIG.SELECTORS.LOGIN)
+
+  const errorDisplay = document.createElement("div")
+  errorDisplay.classList.add("error")
+  loginForm.appendChild(errorDisplay)
+
+  return errorDisplay
+}
+
+initializeLoginPage();
